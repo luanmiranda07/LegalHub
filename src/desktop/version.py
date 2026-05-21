28ad272ffc832a55672acc7d77c38c065bcbbc54
Luan Miranda <133281313+luanmiranda07@users.noreply.github.com>
2026-05-21 15:07:55 -0300

    Add LegalHub version constants

---
+++ src/desktop/version.py
@@ -0,0 +1,7 @@
+APP_NAME = "LegalHub"
+APP_VERSION = "0.1.0"
+
+# Configure no .env local do usuário interno:
+# LEGALHUB_UPDATE_URL=https://seudominio.com/updates/latest.json
+# LEGALHUB_API_BASE_URL=https://seudominio.com
+# LEGALHUB_CLIENT_TOKEN=token_do_usuario_ou_empresa
